220a585fb992cce88fb9f1356b80c7d95538199b
artemmakarcev <makarcev4@gmail.com>
2022-12-30 16:17:14 +0500

    addeed onClick. ref and VirtualDOM

--- src/componets/Profile/Posts/Posts.jsx
+++ src/componets/Profile/Posts/Posts.jsx
@@ -7,15 +7,21 @@ const Posts = (props) => {
     return <Post key={id} title={title} src={src} likesCount={likesCount} />;
   });
 
+  let newPostElement = React.createRef();
+
+  const addPost = () => {
+    console.log("function add post", newPostElement.current.value);
+  };
+
   return (
     <div className={styles.postsBlock}>
       <h3> My post</h3>
       <div>
         <div>
-          <textarea defaultValue="New post" />
+          <textarea ref={newPostElement} defaultValue="New post" />
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={styles.posts}>{postsElements}</div>
