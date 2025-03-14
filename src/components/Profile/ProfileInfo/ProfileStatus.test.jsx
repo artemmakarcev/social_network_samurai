c1bafa3753a1041a9e8776006b6b5417f6b295ce
Artem <makarcev4@gmail.com>
2025-03-14 17:06:27 +0500

    testing vitest jest-dom

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { describe, expect, test } from "vitest";
-import { render } from "vitest-browser-react";
+// import { render } from "vitest-browser-react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import ProfileStatus from "./ProfileStatus";
 
 describe("ProfileStatus component", () => {
   test("should initialize state with props", async () => {
     const initialStatus = "Testing status";
     const { getByText, getByRole } = render(<ProfileStatus initialStatus={initialStatus} />);
-    await expect.element(getByText(`Status:${initialStatus}`)).toBeInTheDocument();
+    await expect.element(getByText(`${initialStatus}`)).toBeInTheDocument();
   });
 });
